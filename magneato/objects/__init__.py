703b986d51b17ae4f6542285f59a02301a1f9846
gtstd <douglass.steven@gmail.com>
2017-06-04 12:36:42 -0400

    Setup (#1)
    * added venv and MANIFEST to gitignore
    
    * added fish-based virtual environment
    
    * added preliminary package structure

---
+++ magneato/objects/__init__.py
@@ -0,0 +1,4 @@
+"""The objects package contains various objects which can be modelled 
+in magneato.
+
+"""
---
+++ setup_venv.fish
@@ -0,0 +1,23 @@
+# create an activation function
+function activate
+    set -gx USER_PATH $PATH
+    set -gx VENV_PROMPT "(venv)"
+    set -gx PATH $PWD/venv/bin /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin
+end
+
+# create a deactivate function
+function deactivate
+    set -gx PATH $USER_PATH
+    set -e USER_PATH
+    set -e VENV_PROMPT
+end
+       
+# setup the virtual environment
+conda create -y -p ./venv \
+    python=3.6 \
+    numpy      \
+    scipy      \
+    h5py
+
+# activate the environment
+activate
